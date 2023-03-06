c19feb84fce64fbe1517030c876bc15b8de4356b
Rubyat <rhkoly@gmail.com>
2023-03-06 19:24:52 +0100

    add file via upload

---
+++ Peer-graded Assignment.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# RUBYAT UL HASNAT\
+# Student
+
+# *I am interested in data science because this is the most exciting field in today's world*
+
+# ### Following code will show how the print function works
+
+# In[1]:
+
+
+print('Hello! This is my first code for the assignment')
+
+
+# #### bulleted list 
+# - apple
+# - mango
+# - banana
+# 
+# #### numbered list
+# 1. apple
+# 2. mango
+# 3. banana
+# 
+# #### table
+# 
+# | id | Fruit  |
+# |------|------|
+# |   1  | apple|
+# |   2  | mango|
+# |   3  | banana|
+# 
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
